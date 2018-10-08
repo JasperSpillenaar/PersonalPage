197cc050d365c616880c80c2a7790c1ae22129cc
jasper spillenaar <1033547@idcollege.nl>
2018-10-08 14:00:21 +0200

    made banner

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,29 @@
+let b = 125;
+let w = 255;
 
+function setup() {
+  createCanvas(1519.2, 1110);  
+}
+
+function draw() {
+  r = map(mouseX, 0, 1519.2, 50, 230);
+  b = map(mouseX, 0, 1519.2, 230, 50);
+  background(b, w);
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
